5674334375f2105ae6dfc67f18f471b03d3943e7
aviad <arielaviad1@gmail.com>
2019-06-28 13:53:06 +0300

    add build folder

---
+++ client/build/precache-manifest.6fdf57cbe44e9fad0f2215aa86096b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d08bb91d5fefd99957f8450f94c9332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "320290fcecf4e4f20347",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d2cc0b835069928bd097",
+    "url": "/static/css/main.e8753073.chunk.css"
+  },
+  {
+    "revision": "320290fcecf4e4f20347",
+    "url": "/static/js/2.44c628fc.chunk.js"
+  },
+  {
+    "revision": "d2cc0b835069928bd097",
+    "url": "/static/js/main.367e16e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
