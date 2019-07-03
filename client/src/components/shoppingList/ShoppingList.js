8c373bffcdb4599549175f13734f82742152e107
aviad <arielaviad1@gmail.com>
2019-07-03 15:51:39 +0300

    Guest alert added

--- client/src/components/shoppingList/ShoppingList.js
+++ client/src/components/shoppingList/ShoppingList.js
@@ -5,7 +5,8 @@ import {
   ListGroupItem,
   Button,
   Row,
-  Col
+  Col,
+  Alert
 } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ItemModal from "../ItemModal/ItemModal";
@@ -49,133 +50,168 @@ const ShoppingList = ({
     }
   }, [user]);
 
-  return (
-    <Container>
-      <Button color="dark" style={{ marginBottom: "2rem" }} onClick={toggle}>
-        Add Item
-      </Button>
-      <EditItemModal
-        EditModal={EditModal}
-        SetEditModal={SetEditModal}
-        EditName={EditName}
-        EditQuantity={EditQuantity}
-        SetEditQuantity={SetEditQuantity}
-      />
-      <ItemModal modal={modal} SetModal={SetModal} />
-      <ListGroup>
-        <MediaQuery maxDeviceWidth={550}>
-          {matches => {
-            if (matches) {
-              return (
-                <TransitionGroup className="shopping-list">
-                  {item.items.map(({ _id, name, quantity, date }) => (
-                    <CSSTransition key={_id} timeout={500} classNames="fade">
-                      <ListGroupItem>
-                        <Row>
-                          <Col xs="1">
-                            <Button
-                              style={{ marginTop: "1.8rem" }}
-                              className="remove-btn"
-                              color="danger"
-                              size="sm"
-                              onClick={() => {
-                                deleteItem(user._id, name);
-                                getItems(user._id);
-                              }}
-                            >
-                              &times;
-                            </Button>
-                          </Col>
-                          <Col style={{ marginTop: "2rem" }} xs="4">
-                            {name}
-                          </Col>
-                          <Col
-                            style={{ marginTop: "0.2rem", textAlign: "center" }}
-                            xs="2"
-                          >
-                            {"Last Update: " + new Date(date).toLocaleString()}
-                          </Col>
-                          <Col
-                            style={{ marginTop: "2rem", paddingLeft: "4rem" }}
-                            xs="3"
-                          >
-                            <Button
-                              color="link"
-                              size="sm"
-                              onClick={() => {
-                                SetEditName(name);
-                                SetEditQuantity(quantity);
-                                deleteItem(user._id, name);
-                                getItems(user._id);
-                                toggleEdit();
-                              }}
-                            >
-                              {quantity}
-                            </Button>
-                          </Col>
-                        </Row>
-                      </ListGroupItem>
-                    </CSSTransition>
-                  ))}
-                </TransitionGroup>
-              );
-            } else {
-              return (
-                <TransitionGroup className="shopping-list">
-                  {item.items.map(({ _id, name, quantity, date }) => (
-                    <CSSTransition key={_id} timeout={500} classNames="fade">
-                      <ListGroupItem>
-                        <Row>
-                          <Col xs="1">
-                            <Button
-                              style={{}}
-                              className="remove-btn"
-                              color="danger"
-                              size="sm"
-                              onClick={() => {
-                                deleteItem(user._id, name);
-                                getItems(user._id);
-                              }}
-                            >
-                              &times;
-                            </Button>
-                          </Col>
-                          <Col style={{ marginTop: "0.2rem" }} xs="3">
-                            {name}
-                          </Col>
-                          <Col
-                            style={{ marginTop: "0.2rem", textAlign: "center" }}
-                            xs="6"
-                          >
-                            {"Last Update: " + new Date(date).toLocaleString()}
-                          </Col>
-                          <Col xs="2">
-                            <Button
-                              color="link"
-                              size="sm"
-                              onClick={() => {
-                                SetEditName(name);
-                                SetEditQuantity(quantity);
-                                deleteItem(user._id, name);
-                                getItems(user._id);
-                                toggleEdit();
-                              }}
-                            >
-                              {quantity}
-                            </Button>
-                          </Col>
-                        </Row>
-                      </ListGroupItem>
-                    </CSSTransition>
-                  ))}
-                </TransitionGroup>
-              );
-            }
-          }}
-        </MediaQuery>
-      </ListGroup>
-    </Container>
-  );
+  const auth = () => {
+    if (isAuth) {
+      return (
+        <Container>
+          <Button
+            color="dark"
+            style={{ marginBottom: "2rem" }}
+            onClick={toggle}
+          >
+            Add Item
+          </Button>
+          <EditItemModal
+            EditModal={EditModal}
+            SetEditModal={SetEditModal}
+            EditName={EditName}
+            EditQuantity={EditQuantity}
+            SetEditQuantity={SetEditQuantity}
+          />
+          <ItemModal modal={modal} SetModal={SetModal} />
+          <ListGroup>
+            <MediaQuery maxDeviceWidth={550}>
+              {matches => {
+                if (matches) {
+                  return (
+                    <TransitionGroup className="shopping-list">
+                      {item.items.map(({ _id, name, quantity, date }) => (
+                        <CSSTransition
+                          key={_id}
+                          timeout={500}
+                          classNames="fade"
+                        >
+                          <ListGroupItem>
+                            <Row>
+                              <Col xs="1">
+                                <Button
+                                  style={{ marginTop: "1.8rem" }}
+                                  className="remove-btn"
+                                  color="danger"
+                                  size="sm"
+                                  onClick={() => {
+                                    deleteItem(user._id, name);
+                                    getItems(user._id);
+                                  }}
+                                >
+                                  &times;
+                                </Button>
+                              </Col>
+                              <Col style={{ marginTop: "2rem" }} xs="4">
+                                {name}
+                              </Col>
+                              <Col
+                                style={{
+                                  marginTop: "0.2rem",
+                                  textAlign: "center"
+                                }}
+                                xs="2"
+                              >
+                                {"Last Update: " +
+                                  new Date(date).toLocaleString()}
+                              </Col>
+                              <Col
+                                style={{
+                                  marginTop: "2rem",
+                                  paddingLeft: "4rem"
+                                }}
+                                xs="3"
+                              >
+                                <Button
+                                  color="link"
+                                  size="sm"
+                                  onClick={() => {
+                                    SetEditName(name);
+                                    SetEditQuantity(quantity);
+                                    deleteItem(user._id, name);
+                                    getItems(user._id);
+                                    toggleEdit();
+                                  }}
+                                >
+                                  {quantity}
+                                </Button>
+                              </Col>
+                            </Row>
+                          </ListGroupItem>
+                        </CSSTransition>
+                      ))}
+                    </TransitionGroup>
+                  );
+                } else {
+                  return (
+                    <TransitionGroup className="shopping-list">
+                      {item.items.map(({ _id, name, quantity, date }) => (
+                        <CSSTransition
+                          key={_id}
+                          timeout={500}
+                          classNames="fade"
+                        >
+                          <ListGroupItem>
+                            <Row>
+                              <Col xs="1">
+                                <Button
+                                  style={{}}
+                                  className="remove-btn"
+                                  color="danger"
+                                  size="sm"
+                                  onClick={() => {
+                                    deleteItem(user._id, name);
+                                    getItems(user._id);
+                                  }}
+                                >
+                                  &times;
+                                </Button>
+                              </Col>
+                              <Col style={{ marginTop: "0.2rem" }} xs="3">
+                                {name}
+                              </Col>
+                              <Col
+                                style={{
+                                  marginTop: "0.2rem",
+                                  textAlign: "center"
+                                }}
+                                xs="6"
+                              >
+                                {"Last Update: " +
+                                  new Date(date).toLocaleString()}
+                              </Col>
+                              <Col xs="2">
+                                <Button
+                                  color="link"
+                                  size="sm"
+                                  onClick={() => {
+                                    SetEditName(name);
+                                    SetEditQuantity(quantity);
+                                    deleteItem(user._id, name);
+                                    getItems(user._id);
+                                    toggleEdit();
+                                  }}
+                                >
+                                  {quantity}
+                                </Button>
+                              </Col>
+                            </Row>
+                          </ListGroupItem>
+                        </CSSTransition>
+                      ))}
+                    </TransitionGroup>
+                  );
+                }
+              }}
+            </MediaQuery>
+          </ListGroup>
+        </Container>
+      );
+    } else {
+      return (
+        <Alert style={{ textAlign: "center" }} color="danger">
+          Please Login/Register To Start!
+        </Alert>
+      );
+    }
+  };
+
+  return auth();
 };
 
 ShoppingList.PropsTypes = {
